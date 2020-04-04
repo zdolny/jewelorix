e9ea06ec42d8ef975b0c20b8bb2c2dcd0670892b
Michał Dolny <michal.dolny@gmail.com>
2020-04-04 22:12:43 +0200

    splash screen

--- js/game.js
+++ js/game.js
@@ -1,1 +1,12 @@
 const jewel = {};
+
+jewel.game = {
+  showScreen(screenId) {
+      const prevActiveScreen = document.querySelector('.screen.active');
+      const activeScreen = document.querySelector(`[data-screen="${screenId}"]`);
+      if (prevActiveScreen) {
+        prevActiveScreen.classList.remove('active');
+      }
+      activeScreen.classList.add('active');
+  }
+};
